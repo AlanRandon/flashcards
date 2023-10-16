a7b0cc56034481ef62980f84752fc2b89bbb9c4a
Alan Randon <alan.randon@outlook.com>
2023-10-16 19:14:28 +0100

    Start work on showing only first letters

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,34 @@
-import "htmx.org"
+import "htmx.org";
 
-// document.addEventListener("htmx:after-settle", (event) => {
-// 	const target = event.target as HTMLElement;
+function wordify(element: HTMLElement) {
+	if (element.dataset.wordified) {
+		return;
+	}
 
-// 	if (target.tagName.toLowerCase() != "main") {
-// 		return
-// 	}
+	element.dataset.wordified = "true";
 
-// 	document.body.scrollIntoView({ block: "start" })
-// })
+	for (const child of element.childNodes) {
+		if (child instanceof HTMLElement) {
+			wordify(child);
+		}
+
+		if (child instanceof Text) {
+			const template = document.createElement("template");
+			const html =
+				child.textContent?.replace(
+					/[^\s]+/gm,
+					`<span class="word">$&</span>`,
+				) || "";
+			template.innerHTML = html;
+			child.replaceWith(template.content);
+		}
+	}
+}
+
+window.wordify = wordify;
+
+declare global {
+	interface Window {
+		wordify: typeof wordify;
+	}
+}
