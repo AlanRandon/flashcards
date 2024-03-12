9b7ee631fcd52cf4a315e64b30c2382522e23c50
Alan Randon <alan.randon@outlook.com>
2024-03-12 19:06:14 +0000

    Add star background

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,27 @@
-const CARGO_MANIFEST_DIR = require("node:process").env.CARGO_MANIFEST_DIR
+const CARGO_MANIFEST_DIR = require("node:process").env.CARGO_MANIFEST_DIR;
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
-	plugins: {
-		"postcss-import": {},
-		"postcss-url": [{
-			filter: "node_modules/**/*",
-			url: "copy",
-			assetsPath: `${CARGO_MANIFEST_DIR}/dist/static`,
-			useHash: true,
-		}],
-		"tailwindcss": {},
-		"autoprefixer": {},
-		"cssnano": {},
-	}
-}
+  plugins: {
+    "postcss-import": {},
+    "postcss-url": [
+      {
+        filter: "node_modules/**/*",
+        url: "copy",
+        assetsPath: `${CARGO_MANIFEST_DIR}/dist/static`,
+        useHash: true,
+      },
+      {
+        filter: "src/**/*",
+        url: "copy",
+        assetsPath: `${CARGO_MANIFEST_DIR}/dist/static`,
+        useHash: true,
+      },
+    ],
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {},
+  },
+};
 
-module.exports = config
-
+module.exports = config;
